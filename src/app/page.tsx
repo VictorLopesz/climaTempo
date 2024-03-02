bf7e1e271f944350b9cd90b3b8fd42a4fec09eae
VictorLopesz <euvictor_h@hotmail.com>
2024-03-02 16:56:41 -0300

    Consumindo api de tempo e exibindo os dados na tela

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,40 @@
 'use client'
+import Cards from '../components/cards';
 import Image from "next/image";
 import { useRef, useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import axios from 'axios';
+import { useForm } from "react-hook-form";
 
 const Key = "bcdd1ca6b3b6617e908b50f1dc55837f";
 
 export default function Home() {
+
   const inputRef = useRef<HTMLInputElement>(null);
   const [dadosApi, setDadosApi] = useState(null);
+  const { reset } = useForm();
+
 
 
   const fetchWeather = async () => {
     if (inputRef.current) {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputRef.current.value}&units=metric&appid=${Key}`;
-      
       try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error('Falha na busca do Weather Data');
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${inputRef.current.value}&units=metric&appid=${Key}`);
+  
+        if (response.data.cod !== 200) {
+          throw new Error(`Erro ao buscar os dados: ${response.data.message}`);
         }
-        
-        const data = await response.json();
-        setDadosApi(null);
-        setDadosApi(data);
+  
+        setDadosApi(response.data);
       } catch (error) {
-        console.error(error);
+        console.error('Erro na busca:', error);
       }
     }
-  }
+    reset({
+      inputRef: ''
+    });
+  }  
   
-  fetchWeather();
-
   return (
       <div className="bg-[#0F0F0F] h-screen w-full grid place-items-center">
         <div className="bg-[#1D1D1F] w-80 p-3 rounded-lg">
@@ -41,11 +45,14 @@ export default function Home() {
               placeholder="Digite sua Localização"
               className="text-lg border-b p-1 border-[#1B1A1D] bg-[#1B1A1D] text-white" 
             />
-            <button 
-            onClick={fetchWeather}
-            className="m-1 ml-3">
-             <FaSearch className="w-6 h-6 hover:text-[#9c907c] transition ease-in-out delay-150 hover:scale-110 duration-300 active:w-4 active:h-4 active:delay-100"/>
-            </button>
+<button 
+    onClick={fetchWeather}
+    className="m-1 ml-3">
+    <FaSearch className="w-6 h-6 hover:text-[#9c907c] transition ease-in-out delay-150 hover:scale-110 duration-300 active:w-4 active:h-4 active:delay-100"/>
+</button>
+          </div>
+          <div>
+            <Cards dadosApi={dadosApi}/>
           </div>
         </div>
       </div>
---
+++ src/components/cards.tsx
@@ -0,0 +1,190 @@
+'use client'
+import react, { useState } from 'react';
+import sol from '../../public/assets/sol.png'
+import chuva from '../../public/assets/chuva.png'
+import flocodeNeve from '../../public/assets/floco-de-neve.png'
+import nuvens from '../../public/assets/nuvens.png'
+import nevoa from '../../public/assets/nevoa.png'
+import ventos from '../../public/assets/vento.png'
+import chuvisco from '../../public/assets/chuvisco.png'
+import rajadas from '../../public/assets/rajada.png'
+import tempestade from '../../public/assets/tempestade.png'
+import { Fade } from "react-awesome-reveal";
+
+const ClimaTipo = [
+    {
+        type: "Céu limpo",
+        icon: sol,
+        mensagem: [
+            "Vai sair? Não se esqueça do protetor solar",
+        ],
+    },
+    {
+        type: "Chuva",
+        icon: chuva,
+        mensagem: [
+            "Não se esqueça do guarda-chuva quando sair",
+        ],
+    },
+    {
+        type: "Neve",
+        icon: flocodeNeve,
+        mensagem: [
+            "Agasalhe-se, está muito frio lá fora",
+        ],
+    },
+    {
+        type: "Nuvens",
+        icon: nuvens,
+        mensagem: [
+            "Aproveite o clima ameno para uma tarde no parque",
+        ]
+    },
+    {
+        type: "Névoa",
+        icon: nevoa,
+        mensagem: [
+            "Olá, o nevoeiro chegou. Cuidado ao dirigir!",
+        ]
+    },
+    {
+        type: "Ventos",
+        icon:  ventos,
+        mensagem: [
+            "O vento traz um ar fresco e renovador",
+        ]
+    },
+    {
+        type: "Chuvisco",
+        icon: chuvisco,
+        mensagem: [
+            "Leve o guarda-chuva porque a chuva pode apertar",
+        ]
+    },
+    {
+        type: "Tempestade",
+        icon: tempestade,
+        mensagem: [
+            "Proteja-se, está chovendo muito"
+        ]
+    },
+        {
+        type: "Rajada",
+        icon: rajadas,
+        mensagem: [
+            "Cuidado com as rajadas"
+        ]
+    },
+];
+
+
+
+interface Traducoes {
+    [key: string]: string;
+  }
+  
+  const traducoes: Traducoes = {
+    "Clear": "Céu Limpo",
+    "Clouds": "Nuvens",
+    "Rain": "Chuva",
+    "Drizzle": "Chuvisco",
+    "Thunderstorm": "Tempestade",
+    "Snow": "Neve",
+    "Mist": "Névoa",
+    "Smoke": "Fumaça",
+    "Haze": "Neblina",
+    "Dust": "Poeira",
+    "Fog": "Nevoeiro",
+    "Sand": "Areia",
+    "Ash": "Cinzas",
+    "Squall": "Rajadas",
+    "Tornado": "Tornado"
+  };
+      
+
+const Cards = ({dadosApi}:any) => {
+    const [icons, setSelectedType] = useState(null);
+    const descricao = dadosApi && dadosApi.weather[0].main;
+
+    const descricaoTraduzida = descricao && traducoes[descricao];
+    
+    return(
+        <>
+        <div 
+        className="gap-6 mt-10 mb-5"
+        >
+            {dadosApi && (
+                <>
+                <Fade
+                direction="down"
+                duration={4000}
+                >
+                    <p className="text-2xl font-semibold text-white flex items-center justify-center">
+                        {dadosApi.name}, {dadosApi.sys.country}
+                        {ClimaTipo.map((clima, index) => {
+                            if (clima.type === descricaoTraduzida){
+                                return (
+                                    <span key={index} className="ml-2">
+                                    <img 
+                                        src={clima.icon.src}
+                                        alt={clima.type}
+                                        width={35} height={35}
+                                        className="ml-2"
+                                    />
+                                </span>
+                                )
+                            }
+                        })}
+                    </p>
+                </Fade>
+                    <br/>
+                    <div className="grid grid-cols-1 w-full m-2 text-sm">
+                        <Fade delay={1000}>
+                            <p className="text-white">
+                                <span className="font-semibold">Temperatura: </span> 
+                                {dadosApi.main.temp}°C
+                            </p>
+                        </Fade>
+                        <Fade delay={2000}>
+                            <p className="text-white">
+                                <span className="font-semibold">Sensação Térmica: </span> 
+                                {dadosApi.main.feels_like}°C
+                            </p>
+                        </Fade>
+                        <Fade delay={3000}>
+                            <p className="text-white">
+                                <span className="font-semibold">Umidade: </span> 
+                                {dadosApi.main.humidity}%
+                            </p>
+                        </Fade>
+                        <Fade delay={4000}>
+                            <p className="text-white">
+                                <span className="font-semibold">Descrição: </span> 
+                                {descricaoTraduzida}
+                            </p>
+                        </Fade>
+                    </div>
+                    <br/>
+                    <br className="border border-b-[#505050e4]"/>
+                    <div className="flex items-center justify-center">
+                        {ClimaTipo.map((clima, index):any => {
+                            if (clima.type === descricaoTraduzida) {
+                                return (
+                                    <div key={index}>
+                                        <p className="text-white font-medium text-center flex text-sm">
+                                            {clima.mensagem}
+                                        </p>
+                                    </div>
+                                )
+                            }
+                       })}
+                       <br/>
+                    </div>
+                </>
+            )}
+        </div>
+        </>
+    );
+}
+
+export default Cards;
