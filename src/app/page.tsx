7ffddb484e015cec21b844a84e9097f6be45d6fd
Victor.Loperz <euvictor_h@hotmail.com>
2024-03-04 14:55:36 -0300

    modifica estilização e adiciona relógio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 'use client'
 import Cards from '../components/cards';
-import Image from "next/image";
 import { useRef, useState } from "react";
-import { FaSearch, FaSpinner } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 import axios from 'axios';
-import { useForm } from "react-hook-form";
 import { BiLoaderAlt } from 'react-icons/bi';
 import Background from '../components/background';
-import { Fade } from "react-awesome-reveal";
 
 const Key = "bcdd1ca6b3b6617e908b50f1dc55837f";
 
@@ -17,22 +14,20 @@ export default function Home() {
   const [dadosApi, setDadosApi] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-  const { reset } = useForm();
-
 
   const fetchWeather = async () => {
     if (inputRef.current) {
       setLoading(true);
       setError("");
-      reset();
       try {
         const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${inputRef.current.value}&units=metric&appid=${Key}`);
-  
+        
         if (response.data.cod !== 200) {
           throw new Error(`Erro ao buscar os dados: ${response.data.message}`);
         }
-  
+        
         setDadosApi(response.data);
+        inputRef.current.value = "";
       } catch (error) {
         console.error('Erro na busca:', error);
         setError("Localização não encontrada. Por favor, tente novamente.");
@@ -40,42 +35,42 @@ export default function Home() {
         setLoading(false);
       }
     }
-  }  
-  
+  }
+
   return (
     <>
-    <div>
-      <Background />
-      <div style={{ textAlign: 'center' }}>
-      <div className="h-screen w-full grid place-items-center shadow-xl">
-        <div className="bg-[#161617cd] w-80 p-3 rounded-lg border border-sky-600">
-          <div className="flex items-center justify-center">
-            <input 
-            ref={inputRef}
-              type="text"
-              placeholder="Digite sua Localização"
-              className="text-sm border border-sky-600 rounded-md p-1 bg-[#1b1a1da8] text-white" 
-            />
-            <div className="">
-            <button 
-              onClick={fetchWeather}
-              className="m-1 ml-3">
-                {loading ? (
-                  <BiLoaderAlt className="animate-spin text-sky-600 w-6 h-6" />
-                ) : (
-                  <FaSearch className="text-sky-600 w-6 h-6 mt-1 hover:text-sky-800 transition ease-in-out delay-150 duration-300 active:duration-75 active:transition active:ease-in-out" />
-                )}
-            </button>
+      <div>
+        <Background />
+        <div style={{ textAlign: 'center' }}>
+          <div className="font-roboto h-screen grid place-items-center shadow-xl">
+            <div className="bg-gradient-to-r from-[#8585E6] to-[#404096] w-80 p-3 rounded-xl ">
+              <div className="flex items-center justify-center">
+                <input
+                  ref={inputRef}
+                  type="text"
+                  placeholder="Digite sua Localização"
+                  className="text-sm rounded-md p-3 bg-[#1b1a1da8] text-white w-full"
+                />
+                <div className="">
+                  <button
+                    onClick={fetchWeather}
+                    className="m-1 ml-3">
+                    {loading ? (
+                      <BiLoaderAlt className="animate-spin text-[#71B5BC] w-6 h-6" />
+                    ) : (
+                      <FaSearch className="text-[#71B5BC] w-6 h-6 mt-1 hover:text-sky-800 transition ease-in-out delay-150 duration-300 active:duration-75 active:transition active:ease-in-out" />
+                    )}
+                  </button>
+                </div>
+              </div>
+              {error !== "" && <p className="text-red-400 text-sm mt-2 flex items-center justify-center">{error}</p>}
+              <div>
+                <Cards dadosApi={dadosApi} />
+              </div>
             </div>
-          </div>
-          {error !== "" && <p className="text-red-400 text-sm mt-2 flex items-center justify-center">{error}</p>}
-          <div>
-            <Cards dadosApi={dadosApi}/>
           </div>
         </div>
       </div>
-      </div>
-    </div>
     </>
   );
 }
--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,5 +1,5 @@
 'use client'
-import react, { useState } from 'react';
+import react, { useEffect, useState } from 'react';
 import sol from '../../public/assets/sol.png'
 import lua from '../../public/assets/lua.png'
 import chuva from '../../public/assets/chuva.png'
@@ -13,65 +13,66 @@ import tempestade from '../../public/assets/tempestade.png'
 import { Fade } from "react-awesome-reveal";
 
 
-const Cards = ({dadosApi}:any) => {
+const Cards = ({ dadosApi }: any) => {
     const horaAtual = new Date().getHours();
-    
+    const [datahora, setDatahora] = useState(new Date().toLocaleTimeString());
+
     const ClimaTipo = [
         {
             type: "Céu Limpo",
-            icon: horaAtual >= 6 && horaAtual <= 18  ? sol : lua,
-            mensagem: horaAtual >= 6 && horaAtual <= 18  ? "Vai sair? Não se esqueça do protetor solar" : "Tenha uma boa noite, durma bem!",
+            icon: sol,
+            mensagem: horaAtual >= 6 && horaAtual <= 18 ? "Vai sair? Não se esqueça do protetor solar" : "Hoje teremos um dia lindo",
         },
         {
             type: "Chuva",
             icon: chuva,
-            mensagem: horaAtual >= 6 && horaAtual <= 18  ? "Não se esqueça do guarda-chuva quando sair" : "Que delicia dormir com barulho da chuva"
+            mensagem: horaAtual >= 6 && horaAtual <= 18 ? "Não se esqueça do guarda-chuva quando sair" : "Que delicia dormir com barulho da chuva"
         },
         {
             type: "Neve",
             icon: flocodeNeve,
-            mensagem: horaAtual >= 6 && horaAtual <= 18  ? "Agasalhe-se, está muito frio lá fora" : "Seu cobertor está quentinho, que inveja",
+            mensagem: horaAtual >= 6 && horaAtual <= 18 ? "Agasalhe-se, está muito frio lá fora" : "Seu cobertor está quentinho, que inveja",
         },
         {
             type: "Nuvens",
             icon: nuvens,
-            mensagem: horaAtual >= 6 && horaAtual <= 18  ? "Aproveite o clima ameno para uma tarde no parque" : "O céu está com nuvens e é lindo à noite"
+            mensagem: horaAtual >= 6 && horaAtual <= 18 ? "Aproveite o clima ameno, respire" : "O céu está com nuvens, veja como é lindo"
         },
         {
             type: "Névoa",
             icon: nevoa,
-            mensagem: horaAtual >= 6 && horaAtual <= 18  ? "Olá, o nevoeiro chegou. Cuidado ao dirigir!" : "Não dá para enxergar nada essa hora, fique em casa"
+            mensagem: horaAtual >= 6 && horaAtual <= 18 ? "Olá, o nevoeiro chegou. Cuidado ao dirigir!" : "Olá, o nevoeiro chegou. Cuidado ao dirigir!"
         },
         {
             type: "Ventos",
-            icon:  ventos,
-            mensagem: horaAtual >= 6 && horaAtual <= 18  ? "O vento traz um ar fresco e renovador" : "Barulho estranho, né? É só o vento na sua porta, relaxe e descanse"
+            icon: ventos,
+            mensagem: horaAtual >= 6 && horaAtual <= 18 ? "O vento traz um ar fresco e renovador" : "Barulho estranho, né? É só o vento na sua porta, relaxe e descanse"
         },
         {
             type: "Chuvisco",
             icon: chuvisco,
-            mensagem:  horaAtual >= 6 && horaAtual <= 18  ? "Leve o guarda-chuva porque a chuva pode apertar" : "Tire a roupa da corda antes de dormir; a chuva pode apertar"
+            mensagem: horaAtual >= 6 && horaAtual <= 18 ? "Leve o guarda-chuva porque a chuva pode apertar" : "Tire a roupa da corda; a chuva pode apertar"
         },
         {
             type: "Tempestade",
             icon: tempestade,
-            mensagem: horaAtual >= 6 && horaAtual <= 18  ? "Proteja-se, está chovendo muito" : "Fique alerta, a chuva pode ser perigosa"
+            mensagem: horaAtual >= 6 && horaAtual <= 18 ? "Proteja-se, está chovendo muito" : "Fique alerta, a chuva pode ser perigosa"
         },
-            {
+        {
             type: "Rajada",
             icon: rajadas,
-            mensagem: 
+            mensagem:
                 "Cuidado com as rajadas"
         },
     ];
-    
-    
-    
+
+
+
     interface Traducoes {
         [key: string]: string;
-      }
-      
-      const traducoes: Traducoes = {
+    }
+
+    const traducoes: Traducoes = {
         "Clear": "Céu Limpo",
         "Clouds": "Nuvens",
         "Rain": "Chuva",
@@ -87,95 +88,116 @@ const Cards = ({dadosApi}:any) => {
         "Ash": "Cinzas",
         "Squall": "Rajadas",
         "Tornado": "Tornado"
-      };
-          
-    const [icons, setSelectedType] = useState(null);
+    };
+
     const descricao = dadosApi && dadosApi.weather[0].main;
     const descricaoTraduzida = descricao && traducoes[descricao];
-    
-    return(
+
+    useEffect(() => {
+        const intervalID = setInterval(() => {
+            setDatahora(new Date().toLocaleTimeString());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalID);
+        };
+    }, []);
+
+    return (
         <>
-        <Fade
-        duration={1000}
-        >
-        <div 
-        className="gap-6 mt-10 mb-5"
-        >
-            {dadosApi && (
-                <>
-                <Fade
-                direction="down"
+            <Fade
                 duration={1000}
+            >
+                <div
+                    className="gap-6 mt-3 mb-5"
                 >
-                    <p className="text-2xl font-semibold text-white flex items-center justify-center">
-                        {dadosApi.name}, {dadosApi.sys.country}
-                        {ClimaTipo.map((clima, index) => {
-                            if (clima.type === descricaoTraduzida){
-                                return (
-                                    <span key={index} className="ml-2">
-                                    <img 
-                                        src={clima.icon.src}
-                                        alt={clima.type}
-                                        width={35} height={35}
-                                        className="ml-2"
-                                    />
-                                </span>
-                                )
-                            }
-                        })}
-                    </p>
-                </Fade>
-                    <br/>
-                    <div className="grid grid-cols-1 w-full text-left m-2 text-sm">
-                        <Fade delay={500}>
-                            <p className="text-white">
-                                <span className="font-semibold">Temperatura: </span> 
-                                {dadosApi.main.temp}°C
-                            </p>
-                        </Fade>
-                        <Fade delay={600}>
-                            <p className="text-white">
-                                <span className="font-semibold">Sensação Térmica: </span> 
-                                {dadosApi.main.feels_like}°C
-                            </p>
-                        </Fade>
-                        <Fade delay={700}>
-                            <p className="text-white">
-                                <span className="font-semibold">Umidade: </span> 
-                                {dadosApi.main.humidity}%
-                            </p>
-                        </Fade>
-                        <Fade delay={800}>
-                            <p className="text-white">
-                                <span className="font-semibold">Descrição: </span> 
-                                {descricaoTraduzida}
-                            </p>
-                        </Fade>
-                    </div>
-                    <br/>
-                    <br className="border border-b-[#505050e4]"/>
-                    <Fade delay={1000}
-                    direction="up"
-                    >
-                    <div className="flex items-center justify-center">
-                        {ClimaTipo.map((clima, index):any => {
-                            if (clima.type === descricaoTraduzida) {
-                                return (
-                                    <div key={index}>
-                                        <p className="text-white font-medium text-center flex text-sm">
-                                            {clima.mensagem}
+                    {dadosApi && (
+                        <>
+                            <div className="text-center flex items-center justify-between w-auto">
+                                <p className="text-[12px] bg-[#39398B] shadow-md rounded-full p-2 text-white">
+                                    {dadosApi.name}, {dadosApi.sys.country}
+                                </p>
+                                <p className="mr-4 mt-1">
+                                    {datahora}
+                                </p>
+                            </div>
+                            <br />
+                            <Fade
+                                direction="down"
+                                duration={1000}
+                            >
+                                <p className="flex items-center justify-center">
+                                    {ClimaTipo.map((clima, index) => {
+                                        if (clima.type === descricaoTraduzida) {
+                                            return (
+                                                <span key={index} className="ml-2">
+                                                    <img
+                                                        src={clima.icon.src}
+                                                        alt={clima.type}
+                                                        width={140} height={140}
+                                                    />
+                                                </span>
+                                            )
+                                        }
+                                    })}
+                                </p>
+                            </Fade>
+                            <br />
+                            <div className="grid grid-cols-1 w-full text-left text-sm">
+                                <Fade delay={500}>
+                                    <p className="text-white shadow-2xl font-semibold flex items-center justify-center text-[30px] mb-10">
+                                        {Math.floor(dadosApi.main.temp)}°C
+                                    </p>
+                                </Fade>
+                                <div className="grid grid-cols-3 gap-2 text-center">
+                                    <Fade delay={800}>
+                                        <p className="text-white">
+                                            <span className="font-semibold mb-1">Descrição</span>
+                                            <br />
+                                            {descricaoTraduzida}
                                         </p>
-                                    </div>
-                                )
-                            }
-                        })}
-                       <br/>
-                    </div>
-                    </Fade>
-                </>
-            )}
-        </div>
-        </Fade>
+                                    </Fade>
+                                    <Fade delay={600}>
+                                        <p className="text-white">
+                                            <span className="font-semibold mb-1">Sensação Térmica </span>
+                                            <br />
+                                            {Math.floor(dadosApi.main.feels_like)}°C
+                                        </p>
+                                    </Fade>
+                                    <Fade delay={700}>
+                                        <p className="text-white">
+                                            <span className="font-semibold mb-1">Umidade </span>
+                                            <br />
+                                            {dadosApi.main.humidity}%
+                                        </p>
+                                    </Fade>
+                                </div>
+                            </div>
+                            <br />
+                            <br className="border border-b-[#505050e4]" />
+
+                            <Fade delay={1000}
+                                direction="up"
+                            >
+                                <div className="flex items-center justify-center p-2 bg-[#e4e4ea] rounded-xl shadow-2xl">
+                                    {ClimaTipo.map((clima, index): any => {
+                                        if (clima.type === descricaoTraduzida) {
+                                            return (
+                                                <div key={index}>
+                                                    <p className="text-[#353689] font-semibold text-center flex text-sm">
+                                                        {clima.mensagem}
+                                                    </p>
+                                                </div>
+                                            )
+                                        }
+                                    })}
+                                    <br />
+                                </div>
+                            </Fade>
+                        </>
+                    )}
+                </div>
+            </Fade>
         </>
     );
 }
